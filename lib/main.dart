62cc04c88f85b18dcfaa9f3bfa72c86760b2f52a
pyl0a <andriypylio@gmail.com>
2024-07-02 21:27:42 +0200

    Integrate Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
 import 'dart:async';
 
+import 'package:crypto_coins_app/firebase_options.dart';
 import 'package:crypto_coins_app/repositories/coins/abstract_coins_repository.dart';
 import 'package:crypto_coins_app/repositories/coins/coins_repository.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 import 'crypto_coins_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final talker = TalkerFlutter.init();
-  GetIt.I.registerSingleton<Talker>(talker);
-  GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
-    () => CoinsRepository(dio: Dio()),
+  final app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runZonedGuarded(
-    () => runApp(const CryptoCoinsApp()),
-    (error, stackTrace) {
-      GetIt.I<Talker>().handle(error, stackTrace);
-    },
+  GetIt.I.registerSingleton<Talker>(talker);
+  GetIt.I.registerSingleton<Dio>(Dio());
+  GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
+    () => CoinsRepository(dio: GetIt.I<Dio>()),
   );
+  // ignore: avoid_print
+  GetIt.I<Talker>().info(app.options.projectId);
+  GetIt.I.registerSingleton<FirebaseApp>(app);
+
+  runApp(const CryptoCoinsApp());
+
+  // runZonedGuarded(
+  //   () => runApp(const CryptoCoinsApp()),
+  //   (error, stackTrace) {
+  //     GetIt.I<Talker>().handle(error, stackTrace);
+  //   },
+  // );
 }
